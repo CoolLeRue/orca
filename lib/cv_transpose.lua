77dd02d4389d9e94238883365858be5eaef6781e
CoolLeRue <steadsthomas@gmail.com>
2019-12-05 18:18:25 -0800

    CV transpose table
    I think this will work for CV outs. Kind of clunky and I don't fully understand everything.

--- lib/cv_transpose.lua
+++ lib/cv_transpose.lua
@@ -1,12 +1,4 @@
-local crow_cv = function (self, x, y)
-	
-	self.y = y
-	self.x = x
-	self.name = 'crow_cv'
-	self.ports = { {1, 0, 'in-port' }, {2, 0, 'in-octave' }, {3, 0, 'in-note' }, {4, 0, 'in-attack' }, {5, 0, 'in-release' } }
-	self:spawn(self.ports)
-
-	local cv_transpose_table = {
+local cv_transpose_table = {
   ['A'] = 0.833,
   ['a'] = 0.916,
   ['B'] = 1.000,
@@ -70,14 +62,5 @@ local crow_cv = function (self, x, y)
   ['8'] = 4.833,
   ['9'] = 4.916,
 }
-	local channel = util.clamp( self:listen( self.x + 1, self.y ) or 0, 0, 1 )
-	local ocatave = util.clamp( self:listen( self.x + 2, self.y ) or 4, 0, 8 )
-	local n = tostring(self:glyph_at(self.x + 3, self.y)) or 'C'
-	local note = cv_transpose_table[n]
-	local attack = util.clamp( self:listen( self.x + 4, self.y ) or 0, 0, 5 )
-	local release = util.clamp( self:listen( self.x + 5, self.y ) or 1, 0, 5 )
 
-	if self:neighbor(self.x, self.y, '*') then
-		crow.output[(channel + 1)].volts = (octave + note)
-		crow.output[(channel + 2)].ar(attack, release)
-  end
+return cv_transpose_table
