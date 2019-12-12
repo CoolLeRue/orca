c6db3a63bfdf4617a954d850fe46a1f12954eba2
CoolLeRue <steadsthomas@gmail.com>
2019-12-11 22:40:31 -0800

    Update orca.lua

--- orca.lua
+++ orca.lua
@@ -347,7 +347,7 @@ function init()
   
   params:set("bpm", 120)
   params:add_separator()
-  params:add{type = "number", id = "jf.ii mode", min = 0, max = 6, default = 1, action = function(x) crow.ii.jf.mode(x) end}
+  params:add{type = "number", id = "jf.ii mode", min = 0, max = 1, default = 1, action = function(x) crow.ii.jf.mode(x) end}
   params:add_separator()
   params:add_trigger('save_p', "< Save project" )
   params:set_action('save_p', function(x) textentry.enter(orca.save_project,  orca.project) end)
