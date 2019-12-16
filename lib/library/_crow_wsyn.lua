9f4a1bf78df24d30ee8b53f8b83f622bcb31bb09
CoolLeRue <steadsthomas@gmail.com>
2019-12-15 16:22:18 -0800

    Update _crow_wsyn.lua

--- lib/library/_crow_wsyn.lua
+++ lib/library/_crow_wsyn.lua
@@ -100,7 +100,7 @@ local crow_wsyn = function (self, x, y)
 
     
 	if self:neighbor(self.x, self.y, '*') then
-		crow.ii.wsyn.play_note( tot_note /12, level/1 )
+		crow.send("ii.wsyn.play_voice("..channel..", "..tot_note.. /12", level/"..level.." )")
 		redraw()
 	end
 
