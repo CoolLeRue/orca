34edf69077604f4b4d8da0f0f162040c1ec2655a
CoolLeRue <steadsthomas@gmail.com>
2020-05-23 13:40:15 -0700

    Update _midi_in.lua
    trying to make it bang when I hit a key

--- lib/library/_midi_in.lua
+++ lib/library/_midi_in.lua
@@ -3,14 +3,24 @@ local midi_in = function ( self, x, y )
   self.y = y
   self.x = x  
   self.name = 'midi in'
-  self.ports = { {1, 0, 'in-ch'}, {0, 1, 'key-out' } }
+  self.ports = { {1, 0, 'in-ch'}, {2, 0, 'in-x'}, {0, 1, 'key-out' }, {-4, 1, 'bang-out'} }
 
   self:spawn(self.ports)
 
   local ch = self:listen( self.x + 1, self.y ) or 1
-  local out = self.notes[ ( ( self.vars.midi[ch] or 1 ) % 12 ) + 1 ]
+  local keyout = self.notes[ ( ( self.vars.midi[ch] or 1 ) % 12 ) + 1 ]
+  local val = 1
+  if keyout ~= keyout2 then
+  	val = 0 
+  end
+  local keyout2 = keyout
+  local bangout = ( val == 0 ) and '*' or '.'
+  local a = util.clamp(self:listen(self.x + 2, self.y) or 1, 1, 35)
+  local offset_x = a + self.x
+
   
-  self:write( 0, 1, out )
+  self:write( 0, 1, keyout )
+  self:write(0, 1, bangout(offset_x, 0))
   
 end
 
