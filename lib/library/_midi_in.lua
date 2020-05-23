02709fa9baf49cabc150109c1a82453051f1d65e
CoolLeRue <steadsthomas@gmail.com>
2020-05-23 13:45:42 -0700

    Update _midi_in.lua

--- lib/library/_midi_in.lua
+++ lib/library/_midi_in.lua
@@ -3,27 +3,17 @@ local midi_in = function ( self, x, y )
   self.y = y
   self.x = x  
   self.name = 'midi in'
-  self.ports = { {1, 0, 'in-ch'}, {2, 0, 'in-x'}, {0, 1, 'key-out' }, {-4, 1, 'bang-out'} }
+  self.ports = { {1, 0, 'in-ch'}, {0, 1, 'key-out' }}
 
   self:spawn(self.ports)
 
   local ch = self:listen( self.x + 1, self.y ) or 1
-  local keyout = self.notes[ ( ( self.vars.midi[ch] or 1 ) % 12 ) + 1 ]
-  local val = 0
-  if keyout ~= keyout2 then
-  	val = 0 
-  else
-  	val = 1
-  end
-  local keyout2 = keyout
-  local bangout = ( val == 0 ) and '*' or '.'
-  local a = util.clamp(self:listen(self.x + 2, self.y) or 1, 1, 35)
-  local offset_x = a + self.x
+  local out = self.notes[ ( ( self.vars.midi[ch] or 1 ) % 12 ) + 1 ]
+ 
 
   
-  self:write( 0, 1, keyout )
-  self:write(0, 1, bangout(offset_x, 0))
-  
+  self:write( 0, 1, out )
+
 end
 
 return midi_in
